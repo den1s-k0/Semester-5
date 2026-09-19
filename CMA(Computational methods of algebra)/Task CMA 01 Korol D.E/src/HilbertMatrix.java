57edaac054fe91f563d39286ba685328efdfd4e4
den1s-k0 <koroldenis0904@gmail.com>
2026-09-19 09:00:41 +0300

    Create folder CMA

---
+++ CMA(Computational methods of algebra)/Task CMA 01 Korol D.E/src/HilbertMatrix.java
@@ -0,0 +1,19 @@
+public class HilbertMatrix {
+
+    public static double[][] hilbertMatrix(int n) {
+
+    }
+
+    public static double[][] lowZeros(int n) {
+
+    }
+
+    public static double[][] upZeros(int n) {
+
+    }
+
+    public static double[] xResults(int n) {
+
+    }
+
+}
